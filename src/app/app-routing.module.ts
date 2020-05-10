982096f47cf3fa74183d52131fb3e514ee19c191
CyberDrudge <satyamsaxena197@gmail.com>
2020-05-10 16:13:56 +0530

    Initial Upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './shared/login/login.component';
+import { HomeComponent } from './shared/layouts/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailComponent } from './components/products/product-detail/product-detail.component';
+import { CartComponent } from './components/cart/cart.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "cart",
+    component: CartComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "products",
+    component: ProductsComponent
+  },
+  {
+    path: "products/:slug",
+    component: ProductDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { SharedModule } from './shared/shared.module';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    SharedModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
