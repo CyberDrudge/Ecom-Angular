430df26557788354b48c23cbb608df2a2d5b51fa
CyberDrudge <satyamsaxena197@gmail.com>
2020-05-17 00:18:35 +0530

    Implement Address COmponent

--- src/app/components/address/address.component.ts
+++ src/app/components/address/address.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {Router} from "@angular/router"
+import { AddressService } from 'src/app/services/address/address.service';
 
 @Component({
   selector: 'app-address',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address.component.scss']
 })
 export class AddressComponent implements OnInit {
+  addresses: any;
+  billing_address: any;
+  shipping_address: any;
 
-  constructor() { }
+  addressForm = this.fb.group({
+    address: ['', [Validators.required]]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private addressService: AddressService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getAddresses();
   }
 
+  getAddresses(){
+    this.addressService.getAddresses().subscribe(res => {
+      if (res.type == "success"){
+        this.addresses = res.data;
+      } else {
+        this.addresses = null;
+      }
+    });
+  }
+
+  onSubmit(){
+    let billing_address = this.addresses[this.billing_address].id;
+    let shipping_address = this.addresses[this.shipping_address].id;
+    console.log(billing_address, shipping_address);
+    localStorage.setItem("billing_address", JSON.stringify(billing_address));
+    localStorage.setItem("shipping_address", JSON.stringify(shipping_address));
+    this.router.navigate(['/checkout']);
+  }
 }
--- src/app/services/address/address.service.ts
+++ src/app/services/address/address.service.ts
@@ -8,11 +8,18 @@ import { environment } from 'src/environments/environment';
 export class AddressService {
   private apiURL: string = environment.apiUrl;
   addAddressUrl:string = '/checkout/address/create/';
+  getAddressesUrl:string = '/addresses';
   constructor(private httpClient: HttpClient) { }
 
   public addAddress(addressFormData){
     return this.httpClient.post<any>(
       this.apiURL+this.addAddressUrl, addressFormData
     );
+  }
+
+  public getAddresses(){
+    return this.httpClient.get<any>(
+      this.apiURL+this.getAddressesUrl
+    )
   }
 }
